3490a98cef52e0b6c9f8f559641a2114633eeb1a
veeso <christian.visintin@veeso.dev>
2025-01-10 12:19:06 +0100

    fix: tests

--- crates/bitfinity-block-validator/src/lib.rs
+++ crates/bitfinity-block-validator/src/lib.rs
@@ -55,22 +55,10 @@ where
         block: Block,
         transactions: &[Transaction],
     ) -> Result<(), Box<dyn std::error::Error>> {
-        if !self.unsafe_blocks_enabled().await? {
-            tracing::debug!("Unsafe blocks are disabled");
-            return Ok(());
-        }
         let validate_args = self.execute_block(block, transactions)?;
         self.validate_unsafe_block(validate_args).await?;
 
         Ok(())
-    }
-
-    /// Get whether unsafe blocks are enabled.
-    async fn unsafe_blocks_enabled(&self) -> Result<bool, Box<dyn std::error::Error>> {
-        self.evm_client
-            .is_unsafe_blocks_enabled()
-            .await
-            .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
     }
 
     /// Execute block and return validation arguments.
@@ -224,5 +212,217 @@ where
             self.provider_factory.chain_spec(),
         ));
         executor.executor(StateProviderDatabase::new(db))
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use std::{
+        collections::{BTreeMap, HashMap},
+        str::FromStr,
+        sync::Arc,
+    };
+
+    use alloy_consensus::TxEip1559;
+    use alloy_genesis::{Genesis, GenesisAccount};
+    use alloy_primitives::{FixedBytes, B256};
+    use alloy_signer::SignerSync;
+    use alloy_signer_local::PrivateKeySigner;
+    use candid::utils::ArgumentEncoder;
+    use candid::CandidType;
+    use ic_canister_client::CanisterClientResult;
+    use reth_chainspec::{Chain, ChainSpec, EthereumHardfork, ForkCondition, MIN_TRANSACTION_GAS};
+    use reth_db::init_db;
+    use reth_node_types::NodeTypes;
+    use reth_primitives::{EthPrimitives, RecoveredTx, TransactionSigned};
+    use reth_provider::{providers::StaticFileProvider, EthStorage};
+    use reth_trie::EMPTY_ROOT_HASH;
+    use reth_trie_db::MerklePatriciaTrie;
+    use serde::de::DeserializeOwned;
+
+    use super::*;
+
+    const CHAIN_ID: u64 = 1;
+    const INITIAL_BASE_FEE: u64 = 1_000_000_000;
+
+    #[derive(Clone)]
+    struct DummyDb;
+
+    impl NodeTypesWithDB for DummyDb {
+        type DB = std::sync::Arc<reth_db::DatabaseEnv>;
+    }
+
+    impl NodeTypes for DummyDb {
+        type Primitives = EthPrimitives;
+        type ChainSpec = ChainSpec;
+        type StateCommitment = MerklePatriciaTrie;
+        type Storage = EthStorage;
+    }
+
+    #[derive(Clone)]
+    struct DummyCanisterClient;
+
+    #[async_trait::async_trait]
+    impl CanisterClient for DummyCanisterClient {
+        async fn update<T, R>(&self, _method: &str, _args: T) -> CanisterClientResult<R>
+        where
+            T: ArgumentEncoder + Send + Sync,
+            R: DeserializeOwned + CandidType,
+        {
+            unimplemented!()
+        }
+
+        async fn query<T, R>(&self, _method: &str, _args: T) -> CanisterClientResult<R>
+        where
+            T: ArgumentEncoder + Send + Sync,
+            R: DeserializeOwned + CandidType,
+        {
+            unimplemented!()
+        }
+    }
+
+    #[tokio::test]
+    async fn test_should_validate_block() {
+        let signer_pk = PrivateKeySigner::from_str(
+            "cc3c029ee81d3a4122270c09ea1f4f35fed7f60c952ef0765f960de3a67f3439",
+        )
+        .expect("failed to parse private key");
+        let signer = signer_pk.address();
+
+        let block = generate_block(&signer_pk);
+        let txs = block
+            .body
+            .transactions
+            .iter()
+            .map(|_| {
+                let mut did_tx = did::Transaction::default();
+                did_tx.from = signer.into();
+
+                did_tx
+            })
+            .collect::<Vec<_>>();
+
+        let client = EvmCanisterClient::new(DummyCanisterClient);
+
+        let db_dir = tempfile::tempdir().expect("failed to create temp dir");
+        let database =
+            Arc::new(init_db(db_dir.path(), Default::default()).expect("failed to init db"));
+
+        let data_dir = tempfile::tempdir().expect("failed to create temp dir");
+
+        let chainspec = ChainSpec::builder()
+            .chain(Chain::mainnet())
+            .genesis(Genesis {
+                alloc: BTreeMap::from([(
+                    signer,
+                    GenesisAccount {
+                        balance: U256::from(10).pow(U256::from(18)),
+                        ..Default::default()
+                    },
+                )]),
+                ..Default::default()
+            })
+            .with_fork(EthereumHardfork::Frontier, ForkCondition::Block(0))
+            .with_fork(EthereumHardfork::Homestead, ForkCondition::Block(0))
+            .with_fork(EthereumHardfork::Shanghai, ForkCondition::Block(0))
+            .with_fork(EthereumHardfork::Cancun, ForkCondition::Block(0))
+            .build();
+        let provider_factory: ProviderFactory<DummyDb> = ProviderFactory::new(
+            database,
+            Arc::new(chainspec),
+            StaticFileProvider::read_write(data_dir.path()).expect("failed to create provider"),
+        );
+        reth_db_common::init::init_genesis(&provider_factory).expect("failed to init genesis");
+
+        let validator = BitfinityBlockValidator::new(client, provider_factory);
+
+        let execute_result =
+            validator.execute_block(block.clone(), &txs).expect("failed to execute block");
+
+        println!("{execute_result:?}",);
+
+        assert_eq!(execute_result.block_number, block.header.number,);
+        assert_eq!(execute_result.transactions_root, block.header.transactions_root.into());
+    }
+
+    fn generate_block(signer_pk: &PrivateKeySigner) -> reth_primitives::Block {
+        let signer = signer_pk.address();
+        let tx_cost = U256::from(INITIAL_BASE_FEE * MIN_TRANSACTION_GAS);
+        let num_tx = 5;
+
+        let mock_tx = |nonce: u64| -> RecoveredTx {
+            let tx = reth_primitives::Transaction::Eip1559(TxEip1559 {
+                chain_id: CHAIN_ID,
+                nonce,
+                gas_limit: MIN_TRANSACTION_GAS,
+                to: signer.into(),
+                max_fee_per_gas: INITIAL_BASE_FEE as u128,
+                max_priority_fee_per_gas: 1,
+                ..Default::default()
+            });
+            let signature_hash = tx.signature_hash();
+            let signature = signer_pk.sign_hash_sync(&signature_hash).unwrap();
+
+            TransactionSigned::new_unhashed(tx, signature).with_signer(signer)
+        };
+        let signer_balance_decrease = tx_cost * U256::from(num_tx);
+        let initial_signer_balance = U256::from(10).pow(U256::from(18));
+        let transactions: Vec<RecoveredTx> = (0..num_tx).map(mock_tx).collect();
+
+        let receipts = transactions
+            .iter()
+            .enumerate()
+            .map(|(idx, tx)| {
+                Receipt {
+                    tx_type: tx.tx_type(),
+                    success: true,
+                    cumulative_gas_used: (idx as u64 + 1) * MIN_TRANSACTION_GAS,
+                    ..Default::default()
+                }
+                .with_bloom()
+            })
+            .collect::<Vec<_>>();
+
+        let header = reth_primitives::Header {
+            number: 1,
+            parent_hash: FixedBytes::new([0; 32]),
+            gas_used: transactions.len() as u64 * MIN_TRANSACTION_GAS,
+            gas_limit: ChainSpec::default().max_gas_limit,
+            mix_hash: B256::ZERO,
+            base_fee_per_gas: Some(INITIAL_BASE_FEE),
+            transactions_root: alloy_consensus::proofs::calculate_transaction_root(
+                &transactions.clone().into_iter().map(|tx| tx.into_signed()).collect::<Vec<_>>(),
+            ),
+            receipts_root: alloy_consensus::proofs::calculate_receipt_root(&receipts),
+            beneficiary: signer,
+            state_root: reth_trie::root::state_root_unhashed(HashMap::from([(
+                signer,
+                (
+                    reth_revm::primitives::AccountInfo {
+                        balance: initial_signer_balance - signer_balance_decrease,
+                        nonce: num_tx,
+                        ..Default::default()
+                    },
+                    EMPTY_ROOT_HASH,
+                ),
+            )])),
+            // use the number as the timestamp so it is monotonically increasing
+            timestamp: 0,
+            withdrawals_root: Some(alloy_consensus::proofs::calculate_withdrawals_root(&[])),
+            blob_gas_used: Some(0),
+            excess_blob_gas: Some(0),
+            parent_beacon_block_root: None,
+            ..Default::default()
+        };
+
+        reth_primitives::Block {
+            header,
+            body: reth_primitives::BlockBody {
+                transactions: transactions.into_iter().map(|tx| tx.into_signed()).collect(),
+                ommers: Vec::new(),
+                withdrawals: Some(vec![].into()),
+            },
+        }
     }
 }
